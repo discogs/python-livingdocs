f0e2dd6845bbb10706b0814cf5401c70079317ec
accraze <accraze@gmail.com>
2016-08-14 14:33:09 -0700

    test: added HugoDoc header and metadata unittests

---
+++ tests/test_maker.py
@@ -0,0 +1,28 @@
+from docsmaker import HugoDoc
+import unittest
+
+class TestHugoDoc(unittest.TestCase):
+
+    def test_header(self):
+        """
+        Check that the title, tags and 
+        date are correct.
+        """
+        doc = HugoDoc(title="PageOne")
+        header_partial = '+++\ntitle = "PageOne"\n'
+        doc_header = doc.header()
+        self.assertIn(header_partial, doc_header)
+        empty_tags = 'tags = []'
+        self.assertIn(empty_tags, doc_header)
+        datestr = str(doc.date)
+        self.assertIn(datestr, doc_header)
+
+    def test_meta_data(self):
+        """
+        Check that metadata can be
+        added to header.
+        """
+        doc = HugoDoc(title="PageTwo")
+        doc.meta['foo'] = 'bar'
+        header_str = '+++\ntitle = "PageTwo"\nfoo = "bar"\n'
+        self.assertIn(header_str, doc.header())
