4ec0c64930d30818feee42163ef65ea27ada0036
accraze <accraze@gmail.com>
2016-08-21 16:51:41 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='livingdocs',
-    version='0.1.0',
+    version='0.1.1',
     license='BSD',
     description='Create living documentation from BDD specs',
     long_description='%s\n%s' % (
